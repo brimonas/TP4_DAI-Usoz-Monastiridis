bb9695cff7194f6159f32f074754428463994d79
Julieta-8 <48799592@est.ort.edu.ar>
2026-03-30 10:36:45 -0300

    extra

--- src/app.js
+++ src/app.js
@@ -17,5 +17,10 @@ app.get("/horaActual", (req, res) => {
 app.get("/FehcaActual", (req, res) => {
   res.send(FehcaActual());
 });
-
+/*app.use((req, res) => {
+  res.status(404).send(`
+    <h1>Error 404 - Ruta no encontrada</h1>
+    <img src="https://http.cat/404" alt="404 cat">
+  `);
+});*/
 export default app;
